b08ea7ae2053a1ad7544e2f62e1a1a89ec54b76e
Andrew Pisliakov <pislakov@gmail.com>
2022-08-29 16:21:03 +0300

    remove server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './style.css';
+
+const createUser = async (user) => {
+  const rawResponse = await fetch('https://rs-easy-english.herokuapp.com/users', {
+    method: 'POST',
+    headers: {
+      // eslint-disable-next-line quote-props
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  });
+  const content = await rawResponse.json();
+
+  console.log(content);
+};
+
+// eslint-disable-next-line quote-props, quotes
+createUser({ "email": "hello@user.com", "password": "Gfhjkm_123" });
